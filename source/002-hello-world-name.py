5d7ef263a45ec8910c7dcb01e493fac23e3bfc00
Andrea Carratta <github@devandreacarratta.it>
2021-06-18 21:18:25 +0200

    002-hello-world-name.py

---
+++ source/002-hello-world-name.py
@@ -0,0 +1,8 @@
+# python 002-hello-world-name.py
+
+# Acquire name in 'name' 
+# Pay Attention - space in the end on input
+# Otherwise the 'input' will be after '?'
+name = input("What's your name? ")
+
+print("Hello World" , name,"!")
